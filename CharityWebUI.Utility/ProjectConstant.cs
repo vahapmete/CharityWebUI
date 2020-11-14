a43c97f4d1b96fc4049dbe10f89c777e1f697626
vahapmete <vahapmt@gmail.com>
2020-11-14 11:57:31 +0300

    Add project files.

---
+++ CharityWebUI.Utility/ProjectConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CharityWebUI.Utility
+{
+    public static class ProjectConstant
+    {
+        public const string ResultNotFound = "Data Not Found";
+    }
+}
